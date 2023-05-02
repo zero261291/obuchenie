1d3f30d1ceebe2e16a36c76fc34923ddd49134c4
zero261291 <106863172+zero261291@users.noreply.github.com>
2023-05-02 16:29:39 +0300

    test4

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
-print("Hello World!!!")
+lst = [5, 6, 8, 9, 25]
+
+#          №1
+
+result = lst[0]
+for i in lst:
+    result *= i
+
+print(result)
+
+#          №2
+
+print(min(lst))
+
+#          №3
+
+print(max(lst))
+
+#          №4
+
+print(sum(lst)/len(lst))
+
+#          №5
+
+#Не сделал
